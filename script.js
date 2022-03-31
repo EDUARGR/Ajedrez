7d5ae4e2bbef4eeabb72b46517e9278ba606355d
Esteban Nahuel Carrizo <estebancarrizo619@gmail.com>
2022-03-31 17:11:43 -0300

    Marcando movimiento de los peones

--- script.js
+++ script.js
@@ -19,52 +19,60 @@ const   peonWhite_1 = document.querySelector(".peon-white-1"),
 
 // Piezas negras
 peonBlack_1.addEventListener("click",()=>{
-    console.log("activado peon 1");
+    document.querySelector(".a6").classList.add("green"); 
+    document.querySelector(".a5").classList.add("green");
 });
 peonBlack_2.addEventListener("click",()=>{
-    console.log("activado peon 2");
+    document.querySelector(".b6").classList.add("green"); 
+    document.querySelector(".b5").classList.add("green");
 });
 peonBlack_3.addEventListener("click",()=>{
-    console.log("activado peon 3");
+    document.querySelector(".c6").classList.add("green"); 
+    document.querySelector(".c5").classList.add("green");
 });
 peonBlack_4.addEventListener("click",()=>{
-    console.log("activado peon 4");
+    document.querySelector(".d6").classList.add("green"); 
+    document.querySelector(".d5").classList.add("green");
 });
 peonBlack_5.addEventListener("click",()=>{
-    console.log("activado peon 5");
+    document.querySelector(".e6").classList.add("green"); 
+    document.querySelector(".e5").classList.add("green");
 });
 peonBlack_6.addEventListener("click",()=>{
-    console.log("activado peon 6");
+    document.querySelector(".f6").classList.add("green"); 
+    document.querySelector(".f5").classList.add("green");
 });
 peonBlack_7.addEventListener("click",()=>{
-    console.log("activado peon 7");
+    document.querySelector(".g6").classList.add("green"); 
+    document.querySelector(".g5").classList.add("green");
 });
 peonBlack_8.addEventListener("click",()=>{
-    console.log("activado peon 8");
+    document.querySelector(".h6").classList.add("green"); 
+    document.querySelector(".h5").classList.add("green");
 });
 
 // Piezas blancas
-peonWhite_1.addEventListener("Click",()=>{
+peonWhite_1.addEventListener("click",()=>{
     console.log("Activado peon 1");
 });
-peonWhite_2.addEventListener("Click",()=>{
+peonWhite_2.addEventListener("click",()=>{
     console.log("Activado peon 2");
 });
-peonWhite_3.addEventListener("Click",()=>{
+peonWhite_3.addEventListener("click",()=>{
     console.log("Activado peon 3");
 });
-peonWhite_4.addEventListener("Click",()=>{
+peonWhite_4.addEventListener("click",()=>{
     console.log("Activado peon 4");
 });
-peonWhite_5.addEventListener("Click",()=>{
+peonWhite_5.addEventListener("click",()=>{
     console.log("Activado peon 5");
 });
-peonWhite_6.addEventListener("Click",()=>{
+peonWhite_6.addEventListener("click",()=>{
     console.log("Activado peon 6");
 });
-peonWhite_7.addEventListener("Click",()=>{
+peonWhite_7.addEventListener("click",()=>{
     console.log("Activado peon 7");
 });
-peonWhite_8.addEventListener("Click",()=>{
+peonWhite_8.addEventListener("click",()=>{
     console.log("Activado peon 8");
 });
